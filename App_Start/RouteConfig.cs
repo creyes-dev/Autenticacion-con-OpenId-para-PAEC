ba30d1a167c0673e83ea8ec47198f62af419747a
Cristian <creyes.dev@gmail.com>
2020-05-04 23:48:08 -0300

    commit

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ENOHSA_PAEC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Values", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Controllers/SesionController.cs
+++ Controllers/SesionController.cs
@@ -12,16 +12,21 @@ namespace ENOHSA_PAEC.Controllers
     public class SesionController : ApiController
     {
         // GET api/sesion/
-        public string Get()
+        public IEnumerable<System.Security.Claims.Claim> Get()
         {
-            return User.Identity.Name;
+            IEnumerable<System.Security.Claims.Claim> claims = null;
+
+            var identity = (System.Security.Claims.ClaimsIdentity)User.Identity;
+            claims = identity.Claims;
+
+            return claims;
         }
 
         // DELETE: api/ejemplo/
         public void Delete()
         {
             string url = "http://tst.autenticar.gob.ar/auth/realms/" + WebConfigurationManager.AppSettings["Authority"] +
-                         "/protocol/openid-connect/logout?redirect_uri=http://server/app/close-session";
+                         "/protocol/openid-connect/logout";
             System.Uri uri = new System.Uri(url);
             Redirect(uri);
         }
